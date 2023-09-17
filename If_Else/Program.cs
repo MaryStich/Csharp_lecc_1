f9b63075a91f98b7c9dcdcdb6f20f9c2d1d81690
Mary <mariyastch@gmail.com>
2023-09-18 01:44:31 +0300

    If_Else proj

---
+++ If_Else/Program.cs
@@ -0,0 +1,15 @@
+﻿// Особое обращение к "любимчику"
+
+Console.Write("UserName: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "mary")
+{
+    Console.WriteLine("Ура! Это же Маша!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
+
